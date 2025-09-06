6981a25b9aedd3c8be9aa2f802801ec4afae3b30
mohankumart884 <mohankumart884@gmail.com>
2025-09-06 12:07:09 +0530

    after some changes in model

--- leetcoach/src/main/java/model/Questions.java
+++ leetcoach/src/main/java/model/Questions.java
@@ -12,7 +12,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Document(collation = "questions")
+@Document(collection = "questions")
 public class Questions {
 
     @Id

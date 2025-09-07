6bb93c72a3397f181e80926e0cbd78584e57b342
mohankumart884 <mohankumart884@gmail.com>
2025-09-07 14:36:41 +0530

    ater added the filter for solved questions

--- leetcoach/src/main/java/leetcoach_BK/leetcoach/LeetcoachApplication.java
+++ leetcoach/src/main/java/leetcoach_BK/leetcoach/LeetcoachApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(exclude = {
-    org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
+		org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
 })
 public class LeetcoachApplication {
 
